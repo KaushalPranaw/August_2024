7551b40121dc4f360ca93543e3fef45215ea13b3
Pranaw Kaushal <pranaw.kaushal.1997@gmail.com>
2024-08-17 05:34:41 +0530

    17 August

---
+++ src/Array_String/RemoveDuplicatesFromSortedArray.java
@@ -0,0 +1,15 @@
+package Array_String;
+
+import java.util.Arrays;
+
+public class RemoveDuplicatesFromSortedArray {
+    public static void main(String[] args) {
+        int[] nums = {0,0,1,1,1,2,2,3,3,4};
+        System.out.println(new RemoveDuplicatesFromSortedArray().removeDuplicates(nums));
+        System.out.println(Arrays.toString(nums));
+
+    }
+    public int removeDuplicates(int[] nums) {
+
+    }
+}
