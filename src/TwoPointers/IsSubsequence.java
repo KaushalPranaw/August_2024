c11d9598ce05cc36d0d594f49b2d64002f5d6cfb
Pranaw Kaushal <pranaw.kaushal.1997@gmail.com>
2024-09-19 07:12:17 +0530

    September 19

--- src/TwoPointers/IsSubsequence.java
+++ src/TwoPointers/IsSubsequence.java
@@ -7,17 +7,15 @@ public class IsSubsequence {
 
     }
 
-    public boolean isSubsequence(String s, String t) {
-        if (s == null || s.equals(""))
-            return true;
+    private boolean isSubsequence(String s, String t) {
         int index = 0;
         for (int i = 0; i < t.length(); i++) {
-            if (s.charAt(index) == t.charAt(i))
+            if (t.charAt(i) == s.charAt(index)) {
                 index++;
-            if (index == s.length())
+            }
+            if (s.length() == index)
                 return true;
         }
         return false;
-
     }
 }
--- src/TwoPointers/ValidPalindrome.java
+++ src/TwoPointers/ValidPalindrome.java
@@ -7,12 +7,12 @@ public class ValidPalindrome {
 
     }
 
-    public boolean isPalindrome(String s) {
-          /*s = "A man, a plan, a canal: Panama"
+    /*public boolean isPalindrome(String s) {
+     *//*s = "A man, a plan, a canal: Panama"
             s = "a man, a plan, a canal: panama" - after converting all uppercase letters into lowercase letters
             s = "amanaplanacanalpanama" - removing all non-alphanumeric character
             after reverse if same to valid palindrome h
-            */
+            *//*
         StringBuilder alphaNumericString = new StringBuilder();
         for (int i = 0; i < s.length(); i++) {
             char c = s.charAt(i);
@@ -31,6 +31,28 @@ public class ValidPalindrome {
         }
         return true;
 
+    }*/
+
+    public boolean isPalindrome(String s) {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < s.length(); i++) {
+            if (Character.isLetter(s.charAt(i)) || Character.isDigit(s.charAt(i))) {
+                sb.append(s.charAt(i));
+            }
+        }
+        return checkPalindrome(sb.toString().toLowerCase());
+    }
+
+    private boolean checkPalindrome(String s) {
+        int left = 0, right = s.length() - 1;
+        while (left < right) {
+            if (s.charAt(left) != s.charAt(right)) {
+                return false;
+            }
+            left++;
+            right--;
+        }
+        return true;
     }
 
 }
