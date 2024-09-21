e36627658bfdb946d431aca5e634a9174eca971e
Pranaw Kaushal <pranaw.kaushal.1997@gmail.com>
2024-09-21 05:58:20 +0530

    September 20

---
+++ src/Math/PalindromeNumber.java
@@ -0,0 +1,23 @@
+package Math;
+
+public class PalindromeNumber {
+    public static void main(String[] args) {
+        int x = 121;
+        System.out.println(new PalindromeNumber().isPalindrome(x));
+
+    }
+
+    public boolean isPalindrome(int x) {
+        if (x < 0)
+            return false;
+        int temp = x;
+        int reverse = 0;
+        while (temp != 0) {
+            int d = temp % 10;
+            reverse = reverse * 10 + d;
+            temp = temp / 10;
+        }
+        return x == reverse;
+
+    }
+}
