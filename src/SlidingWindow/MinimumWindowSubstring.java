1c95740a357faa952db35b4e25f49f42fb9fc9d3
KaushalPranaw <pranaw.kaushal.1997@gmail.com>
2024-10-20 11:35:01 +0530

    October 20, 2024

--- src/SlidingWindow/MinimumWindowSubstring.java
+++ src/SlidingWindow/MinimumWindowSubstring.java
@@ -11,45 +11,49 @@ public class MinimumWindowSubstring {
     }
 
     public String minWindow(String s, String t) {
-        if (s.length() < t.length() || t.length() == 0)
+        if (s.length() < t.length() || t.length() == 0) {
             return "";
+        }
 
         Map<Character, Integer> map = new HashMap<>();
         for (int i = 0; i < t.length(); i++) {
-            map.put(t.charAt(i), map.getOrDefault(t.charAt(i), 0) + 1);
+            char tChar = t.charAt(i);
+            map.put(tChar, map.getOrDefault(tChar, 0) + 1);
         }
 
-        int left = 0, right = 0;
-        int charTLeft = t.length();
-        int ansIndex = 0, ansLen = Integer.MAX_VALUE;
-
-        while (right < s.length()) {
-            char rChar = s.charAt(right);
+        int l = 0, r = 0;
+        int ansIndex = 0;
+        int ansLen = Integer.MAX_VALUE;
+        int tlen = t.length();
+        while (r < s.length()) {
+            char rChar = s.charAt(r);
             if (map.containsKey(rChar)) {
                 int count = map.get(rChar);
                 if (count > 0) {
-                    charTLeft--;
+                    tlen--;
                 }
                 map.put(rChar, count - 1);
             }
-            right++;
-        }
+            r++;
 
-        while (charTLeft == 0) {
-            if (ansLen > right - left) {
-                ansLen = right - left;
-                ansIndex = left;
-            }
-            char lChar = s.charAt(left);
-            if (map.containsKey(lChar)) {
-                int count = map.get(lChar);
-                if (count == 0) {
-                    charTLeft++;
+            while (tlen == 0)
+            {
+                if (ansLen > r - l) {
+                    ansLen = r - l;
+                    ansIndex = l;
                 }
-                map.put(lChar, count + 1);
+                char lChar = s.charAt(l);
+                if (map.containsKey(lChar)) {
+                    int count = map.get(lChar);
+                    if (count == 0) {
+                        tlen++;
+                    }
+                    map.put(lChar, count + 1);
+                }
+                l++;
             }
-            left++;
         }
         return ansLen == Integer.MAX_VALUE ? "" : s.substring(ansIndex, ansIndex + ansLen);
+
     }
 }
