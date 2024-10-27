d4a74d181047a76230e710fbf3396a2984f189b4
KaushalPranaw <pranaw.kaushal.1997@gmail.com>
2024-10-27 12:33:19 +0530

    October 27, 2024

---
+++ src/stack/MinStack.java
@@ -0,0 +1,57 @@
+package stack;
+
+import java.util.Stack;
+
+public class MinStack {
+    private Stack<Integer> stack;
+    private Stack<Integer> minStack;
+
+    public MinStack() {
+        stack = new Stack<>();
+        minStack = new Stack<>();
+    }
+
+    public void push(int val) {
+        stack.push(val);
+
+        if (minStack.isEmpty() || val <= minStack.peek()) {
+            minStack.push(val);
+        }
+    }
+
+    public void pop() {
+        if (stack.peek().equals(minStack.peek()))
+            minStack.peek();
+        stack.pop();
+    }
+
+    public int top() {
+        return stack.peek();
+    }
+
+    public int getMin() {
+        return minStack.peek();
+    }
+
+    public static void main(String[] args) {
+        // Creating an instance of MinStack
+        MinStack minStack = new MinStack();
+
+        // Demonstrating the push operation
+        minStack.push(-2);
+        minStack.push(0);
+        minStack.push(-3);
+
+        // Getting the minimum element after pushing -2, 0, -3
+        System.out.println("Minimum element: " + minStack.getMin()); // Output: -3
+
+        // Popping the top element (-3)
+        minStack.pop();
+
+        // Getting the top element after pop
+        System.out.println("Top element: " + minStack.top()); // Output: 0
+
+        // Getting the minimum element after popping
+        System.out.println("Minimum element: " + minStack.getMin()); // Output: -2
+    }
+}
---
+++ src/stack/SimplifyPath.java
@@ -0,0 +1,36 @@
+package stack;
+
+import java.util.Stack;
+
+public class SimplifyPath {
+    public static void main(String[] args) {
+        String s = "/home/";
+        System.out.println(new SimplifyPath().simplifyPath(s));
+
+    }
+
+    public String simplifyPath(String path) {
+
+        String components[] = path.split("/");
+
+        Stack<String> stack = new Stack<>();
+
+        for (String component : components) {
+            if (component.isEmpty() || component.equals(".")) {
+                continue;
+            }
+            if (component.equals("..")) {
+                if (!stack.isEmpty()) {
+                    stack.pop();
+                }
+            } else {
+                stack.push(component);
+            }
+        }
+        StringBuilder sb = new StringBuilder();
+        for (String comp : stack) {
+            sb.append("/").append(comp);
+        }
+        return sb.length() > 0 ? sb.toString() : "/";
+    }
+}
