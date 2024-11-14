e288d25975f5dbdc65ec5a34df60a517f97ad502
KaushalPranaw <pranaw.kaushal.1997@gmail.com>
2024-11-14 08:23:59 +0530

    November 12, 2024

--- src/DP_With_TUF/Fibonacci.java
+++ src/DP_With_TUF/Fibonacci.java
@@ -4,7 +4,7 @@ import java.util.Arrays;
 
 public class Fibonacci {
     //Using recur+memo
-    public static void main(String[] args) {
+    /*public static void main(String[] args) {
         int n = 5;
         //Step 0: initialize dp
         int[] dp = new int[n + 1];
@@ -22,5 +22,49 @@ public class Fibonacci {
 
         // Step 1: whatever is computed store them in dp
         return dp[n] = getFibonacci(n - 1, dp) + getFibonacci(n - 2, dp);
+    }*/
+
+    //using bottom up (tabulation)
+    /*public static void main(String[] args) {
+        int n = 5;
+
+        //Step 0: initialize dp
+        int[] dp = new int[n + 1];
+        Arrays.fill(dp, -1);
+        System.out.println(getFibonacci_using_BottomUp(n, dp));
+
+    }
+
+    private static int getFibonacci_using_BottomUp(int n, int[] dp) {
+        //base case
+        dp[0] = 0;
+        dp[1] = 1;
+
+        //if value is greater than 1
+        for (int i = 2; i <= n; i++) {
+            dp[i] = dp[i - 1] + dp[i - 2];
+        }
+
+        return dp[n];
+    }*/
+
+    //Using constant space
+    public static void main(String[] args) {
+        int n = 5;
+        System.out.println(getFibonacci_using_BottomUp(n));
+    }
+
+    private static int getFibonacci_using_BottomUp(int n) {
+        //base case
+        int prevOfPrev = 0;
+        int prev = 1;
+
+        for (int i = 2; i <= n; i++) {
+            int cur_i = prev + prevOfPrev;
+            prevOfPrev = prev;
+            prev = cur_i;
+        }
+
+        return prev;
     }
 }
