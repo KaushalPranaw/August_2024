2a0ce536aa7f124d98113a74219d33fc851a6544
KaushalPranaw <pranaw.kaushal.1997@gmail.com>
2024-11-22 09:41:53 +0530

    November 22, 2024

---
+++ src/Graph_With_TUF/NumberOfIslands.java
@@ -0,0 +1,67 @@
+package Graph_With_TUF;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class NumberOfIslands {
+    public static void main(String[] args) {
+        char[][] grid = {
+                {'0', '1', '1', '1', '0', '0', '0'},
+                {'0', '0', '1', '1', '0', '1', '0'}
+        };
+
+        System.out.println(new NumberOfIslands().numIslands(grid));
+    }
+
+    private int numIslands(char[][] grid) {
+        int m = grid.length;
+        int n = grid[0].length;
+
+        int[][] visited = new int[m][n];
+
+        int count = 0;
+
+        for (int row = 0; row < m; row++) {
+            for (int col = 0; col < n; col++) {
+                if (visited[row][col] == 0 && grid[row][col] == '1') {
+                    bfs(row, col, visited, grid);
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+
+    private void bfs(int row, int col, int[][] visited, char[][] grid) {
+
+        Queue<Pair> queue = new LinkedList<>();
+        queue.add(new Pair(row, col));
+        visited[row][col] = 1;
+
+        int m = grid.length;
+        int n = grid[0].length;
+
+        while (!queue.isEmpty()) {
+            Pair pair = queue.poll();
+
+            // traverse in the neighbours and mark them if its a land
+            for (int delrow = -1; delrow <= 1; delrow++) {
+                for (int delcol = -1; delcol <= 1; delcol++) {
+                    int nRow = pair.row + delrow;
+                    int nCol = pair.col + delcol;
+
+                    // check if neighbour row and column is valid, and is an unvisited land
+                    if (nRow >= 0 && nRow < m && nCol >= 0 && nCol < n
+                            && grid[nRow][nCol] == '1' && visited[nRow][nCol] == 0) {
+                        queue.add(new Pair(nRow, nCol));
+                        visited[nRow][nCol] = 1;
+                    }
+                }
+            }
+        }
+
+    }
+
+    record Pair(int row, int col) {
+    }
+}
---
+++ src/Graph_With_TUF/TopologicalDFS.java
@@ -0,0 +1,51 @@
+package Graph_With_TUF;
+
+import java.util.*;
+
+public class TopologicalDFS {
+    public static void main(String[] args) {
+        List<List<Integer>> adj = new ArrayList<>();
+        for (int i = 0; i < 6; i++) {
+            adj.add(new ArrayList<>());
+        }
+
+
+        adj.get(5).add(2);
+        adj.get(5).add(0);
+        adj.get(4).add(0);
+        adj.get(4).add(1);
+        adj.get(3).add(1);
+        adj.get(2).add(3);
+
+
+        new TopologicalDFS().topoSort(6, adj);
+    }
+
+    private void topoSort(int N, List<List<Integer>> adj) {
+        Stack<Integer> stack = new Stack<>();
+        int[] visited = new int[N];
+
+        for (int i = 0; i < N; i++) {
+            if (visited[i] == 0) {
+                dfs(i, visited, adj, stack);
+            }
+        }
+
+        int topo[] = new int[N];
+        int index = 0;
+        while (!stack.isEmpty()) {
+            topo[index++] = stack.pop();
+        }
+        System.out.println(Arrays.toString(topo));
+    }
+
+    private void dfs(int node, int[] visited, List<List<Integer>> adj, Stack<Integer> stack) {
+        visited[node] = 1;
+        for (int neighbour : adj.get(node)) {
+            if (visited[neighbour] == 0) {
+                dfs(neighbour, visited, adj, stack);
+            }
+        }
+        stack.push(node);
+    }
+}
