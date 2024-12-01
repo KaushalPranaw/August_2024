b0e9fef082c1920c13b1a33376beeaa4e74c5c36
KaushalPranaw <pranaw.kaushal.1997@gmail.com>
2024-12-01 18:23:32 +0530

    December 01, 2024

---
+++ src/DP_With_TUF/DP24_RodCuttingProblem.java
@@ -0,0 +1,98 @@
+package DP_With_TUF;
+
+import java.util.Arrays;
+
+public class DP24_RodCuttingProblem {
+    public static void main(String args[]) {
+        int N = 8; // Total rod length
+        int[] price = {1, 5, 8, 9, 10, 17, 17, 20}; // Prices for lengths 1 to N
+
+        System.out.println("Maximum revenue: " + rodCutting(N, price));
+    }
+/*
+    //recur+memo
+    private static int rodCutting(int length, int[] price) {
+        int n = price.length;
+        int dp[][]=new int[n][length+1];
+        for(int[] row: dp){
+            Arrays.fill(row, -1);
+        }
+
+        return rodCuttingUtil(n - 1, length, price, dp);
+    }
+
+    private static int rodCuttingUtil(int ind, int length, int[] price, int[][] dp) {
+        //base case
+        if (ind == 0) {
+            return length * price[0];
+        }
+        if(dp[ind][length]!=-1){
+            return dp[ind][length];
+        }
+
+        int not_take = rodCuttingUtil(ind - 1, length, price, dp);
+
+        int take = Integer.MIN_VALUE;
+        int rodLength = ind + 1;
+        if (rodLength <= length) {
+            take = price[ind] + rodCuttingUtil(ind, length - rodLength, price, dp);
+        }
+        return dp[ind][length]=Math.max(take, not_take);
+    }*/
+
+
+    /*
+    //tab
+    private static int rodCutting(int N, int[] price) {
+        int n = price.length;
+        int dp[][] = new int[n][N + 1];
+
+        //base case
+        for (int i = 0; i <= N; i++) {
+            dp[0][i] = i * price[0];
+        }
+
+        for (int ind = 1; ind < n; ind++) {
+            for (int length = 0; length <= N; length++) {
+                int not_take = dp[ind - 1][length];
+
+                int take = Integer.MIN_VALUE;
+                int rodLength = ind + 1;
+                if (rodLength <= length) {
+                    take = price[ind] + dp[ind][length - rodLength];
+                }
+                dp[ind][length] = Math.max(take, not_take);
+            }
+        }
+
+        return dp[n - 1][N];
+    }*/
+
+    //space
+    private static int rodCutting(int N, int[] price) {
+        int n = price.length;
+        int prev[] = new int[N + 1];
+
+        //base case
+        for (int i = 0; i <= N; i++) {
+            prev[i] = i * price[0];
+        }
+
+        for (int ind = 1; ind < n; ind++) {
+            int cur[] = new int[N + 1];
+            for (int length = 0; length <= N; length++) {
+                int not_take = prev[length];
+
+                int take = Integer.MIN_VALUE;
+                int rodLength = ind + 1;
+                if (rodLength <= length) {
+                    take = price[ind] + cur[length - rodLength];
+                }
+                cur[length] = Math.max(take, not_take);
+            }
+            prev = cur;
+        }
+
+        return prev[N];
+    }
+}
---
+++ src/DP_With_TUF/DP25_LongestCommonSubsequence.java
@@ -0,0 +1,97 @@
+package DP_With_TUF;
+
+import java.util.Arrays;
+
+public class DP25_LongestCommonSubsequence {
+    public static void main(String args[]) {
+        String s1 = "acd";
+        String s2 = "ced";
+
+        // Call the lcs function and print the result
+        System.out.println("The Length of Longest Common Subsequence is " + lcs(s1, s2));
+    }
+/*
+    //recur+memo
+    private static int lcs(String s1, String s2) {
+        int m = s1.length();
+        int n = s2.length();
+
+        int[][] dp = new int[m][n];
+        // Initialize the dp array with -1 to indicate that subproblems are not solved yet
+        for (int rows[] : dp)
+            Arrays.fill(rows, -1);
+
+        return lcsUtil(s1, s2, m - 1, n - 1, dp);
+    }
+
+    private static int lcsUtil(String s1, String s2, int ind1, int ind2, int[][] dp) {
+        //base case
+        if (ind1 < 0 || ind2 < 0) {
+            return 0;
+        }
+        // If the result for this subproblem has already been calculated, return it
+        if (dp[ind1][ind2] != -1)
+            return dp[ind1][ind2];
+
+        if (s1.charAt(ind1) == s2.charAt(ind2)) {
+            return dp[ind1][ind2] = 1 + lcsUtil(s1, s2, ind1 - 1, ind2 - 1, dp);
+        } else {
+            return dp[ind1][ind2] = Math.max(lcsUtil(s1, s2, ind1 - 1, ind2, dp),
+                    lcsUtil(s1, s2, ind1, ind2 - 1, dp));
+        }
+    }*/
+
+/*
+    //tabulation
+    private static int lcs(String s1, String s2) {
+        int m = s1.length();
+        int n = s2.length();
+
+        int[][] dp = new int[m + 1][n + 1];
+
+        //base case
+        //since ind1 & ind2 can't be negative so we will shift here
+        //ind1==0 || ind2==0 then return 0
+        for (int i = 0; i <= m; i++) {
+            dp[i][0] = 0;
+        }
+        for (int i = 0; i <= n; i++) {
+            dp[0][i] = 0;
+        }
+
+        for (int ind1 = 1; ind1 <= m; ind1++) {
+            for (int ind2 = 1; ind2 <= n; ind2++) {
+                if (s1.charAt(ind1 - 1) == s2.charAt(ind2 - 1)) {
+                    dp[ind1][ind2] = 1 + dp[ind1 - 1][ind2 - 1];
+                } else {
+                    dp[ind1][ind2] = Math.max(dp[ind1][ind2 - 1], dp[ind1 - 1][ind2]);
+                }
+            }
+        }
+
+        return dp[m][n];
+    }*/
+
+
+    //space
+    private static int lcs(String s1, String s2) {
+        int m = s1.length();
+        int n = s2.length();
+
+        int[] prev = new int[n + 1];
+
+        for (int ind1 = 1; ind1 <= m; ind1++) {
+            int[] cur = new int[n + 1];
+            for (int ind2 = 1; ind2 <= n; ind2++) {
+                if (s1.charAt(ind1 - 1) == s2.charAt(ind2 - 1)) {
+                    cur[ind2] = 1 + prev[ind2 - 1];
+                } else {
+                    cur[ind2] = Math.max(cur[ind2 - 1], prev[ind2]);
+                }
+            }
+            prev=cur;
+        }
+
+        return prev[n];
+    }
+}
